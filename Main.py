52f1bb71cd51cdaf300336859b3a9e21565c1652
NAVEEN7036K <62348479+NAVEEN7036K@users.noreply.github.com>
2022-09-28 09:38:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,22 @@
-for x in range(0,len(arr)):
-  smallest = arr[x]
-  for y in range(x+1,len(arr)):
-    if(arr[x]>arr[y]):
-      smallest = arr[y]
-  arr[x], arr[smallest] = arr[smallest],arr[x]
+from typing import List
+
+def selectionSort(array, size) -> List[int]:
+  for i in range(len(arr)):
+    minimum = i
+    for j in range(i+1,len(arr)):
+      if arr[j] < arr[minimum]:
+        minimum = j
+    temp = arr[i]
+    arr[i] = arr[minimum]
+    arr[minimum] = temp
+  return arr
+
+# Do not change the following code
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(selectionSort(data, len(data)))
